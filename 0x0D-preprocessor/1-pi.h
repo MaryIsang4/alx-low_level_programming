807b38283aa2bc62f9d00d4878599bb4e2dae9c2
MaryIsang4 <Maryisang2018@gmail.com>
2022-10-10 14:05:21 -0700

    Task1

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,12 +1,12 @@
-#ifndef PI
-#define PI
+#ifndef PI_H
+#define PI_H
 
 /*
- *  * File: 1-pi.h
- *   * Auth: Isang Mary
- *    * Desc: Header file that defines a macro named PI
- *     *       as an abbreviation for the token 3.14159265359.
- *      */
+ * File: 1-pi.h
+ * Auth: Isang Mary
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
 
 #define PI 3.14159265359
 
