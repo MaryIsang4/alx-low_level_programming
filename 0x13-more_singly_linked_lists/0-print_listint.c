52cd43b7d19a2d6fdc6562138d485f19020db95a
MaryIsang4 <Maryisang2018@gmail.com>
2022-10-26 15:57:47 -0700

    Task0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
 /*
- * File: 0-print_listint.c 
+ * File: 0-print_listint.c
  * Auth: Isang Mary
  */
 
@@ -9,6 +9,7 @@
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: A pointer to the head of the list_t list.
+ *
  * Return: The number of nodes in the list_t list.
  */
 size_t print_listint(const listint_t *h)
